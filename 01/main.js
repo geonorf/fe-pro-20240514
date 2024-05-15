4baa26761bceb94ff5ef546f7694f32f0705c483
Renat <tsygvintsev@srs.kiev.ua>
2024-05-15 22:50:05 +0300

    Add JS, update index

---
+++ 01/main.js
@@ -0,0 +1,32 @@
+// Number
+let Value1 = 1;
+let SomeVal = 123;
+let zero_val = 0;
+
+// Strings
+let user_mail = "tempuser@mail.com";
+let someTextWithMail = `Mail to: ${user_mail}`;
+
+// Display in Console
+console.log(Value1);
+console.log(user_mail);
+console.log(someTextWithMail);
+
+// NaN
+let NanVal = zero_val/zero_val;
+
+// NULL
+let NullVal = null;
+
+console.log(NanVal);
+console.log(NullVal);
+
+// Reuse variables
+Value1 = 50;
+console.log(Value1);
+
+user_mail = "temp2user@gmail.com";
+console.log(user_mail);
+
+
+
