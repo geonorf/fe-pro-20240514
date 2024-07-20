2bed0716316ba3ee1f6e4abe47332012514b1991
Renat <tsygrenat@gmail.com>
2024-07-20 13:59:30 +0300

    change interface

--- 09/task_03/script/main.js
+++ 09/task_03/script/main.js
@@ -3,13 +3,13 @@ function createFifty(motherElem){
         let newElem = document.createElement('div');
         newElem.style.width  = '50px';
         newElem.style.height = '50px';
-        newElem.style.background = 'green';
+        newElem.style.backgroundColor = 'green';
         motherElem.appendChild(newElem);
     }
 }
 
 window.onload = function(){
-    let mElem = document.querySelector('.mContainer');
+    let mElem = document.querySelector('.task_block');
 
     let newContainer = document.createElement('div');
     newContainer.classList.add('cContainer');
@@ -21,11 +21,15 @@ window.onload = function(){
 
     mElem.appendChild(newContainer);
 
-    console.log(mElem);
+    // console.log(mElem);
 
-    [...mElem.childNodes][1].forEach((divElem, index) => {
+    let [...mElemArr] = document.querySelectorAll('.cContainer > div');
+
+    console.log(mElemArr);
+
+    mElemArr.forEach((divElem, index) => {
         divElem.classList.add('circle-elemt');
-        if (!(index%2 === 0)){
+        if (index%2 === 0){
             divElem.classList.add('test');
         }
     });
